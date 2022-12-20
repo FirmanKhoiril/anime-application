471c1728d60a2e35594f9981fc77c9945933c3ec
firmankhoiril <firmankhoiril13@gmail.com>
2022-12-20 10:50:13 +0700

    delete footer

--- src/components/Footer.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { AiFillGithub, AiFillInstagram } from "react-icons/ai";
-const Footer = () => {
-  return <div>Footer</div>;
-};
-
-export default Footer;
