fe4208634f5e797f823cfdd813349a5b58ed4f4e
firmankhoiril <firmankhoiril13@gmail.com>
2023-01-25 18:52:43 +0700

    change card image

--- src/components/AnimeCard.jsx
+++ src/components/AnimeCard.jsx
@@ -5,11 +5,11 @@ import LocalFireDepartmentIcon from "@mui/icons-material/LocalFireDepartment";
 
 const AnimeCard = ({ anime }) => {
   return (
-    <Card sx={{ width: { md: "290px", xs: "290px", sm: "100%" } }} className="rounded-xl md:ml-16 sm:ml-20 xs:ml-10 ml-5   shadow-lg shadow-black/70 hover:scale-[1.01] hover:ease-in transition duration-700">
+    <Card sx={{ width: { md: "290px", xs: "290px", sm: "100%" } }} className="rounded-3xl md:ml-16 sm:ml-20 xs:ml-10 ml-5   shadow-lg dark:shadow-white/80 shadow-black/70 hover:scale-[1.01] hover:ease-in transition duration-700">
       <Link to={`/anime/${anime._id}`}>
-        <CardMedia component={"img"} title={anime.title} sx={{ width: { xs: "290px", md: "290px", sm: "100%" }, height: 370 }} image={anime.image} draggable />
+        <CardMedia component={"img"} title={anime.title} sx={{ width: { xs: "290px", md: "290px", sm: "100%" }, height: 320 }} image={anime.image} draggable />
       </Link>
-      <CardContent className=" bg-white/10 dark:bg-[#1e1e1e] h-[106px]">
+      <CardContent className=" bg-white/10 dark:bg-dark h-[100px]">
         <Link to={`/anime/${anime._id}`}>
           <Typography fontWeight={"bold"} variant="subtitle1" className="truncate text-slate-600 dark:text-slate-300">
             {anime.title}
