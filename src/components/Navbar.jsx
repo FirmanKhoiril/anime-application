e1dec0805869bbba809cd393fe89aa01c74e8b49
firmankhoiril <firmankhoiril13@gmail.com>
2023-01-25 19:34:40 +0700

    change logo and preview logo website

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Stack } from "@mui/material";
 import { Link } from "react-router-dom";
-import Logo from "../assets/anime.png";
+import Logo from "../assets/logoreeal.png";
 import { SearchBar } from "./";
 
 const Navbar = () => {
@@ -9,7 +9,7 @@ const Navbar = () => {
     <Stack direction="row" className="items-center sticky top-0 justify-between bg-white/80 dark:bg-[#20232A] drop-shadow-lg z-50 " p={2}>
       <Link to="/" className="flex items-center">
         <p className="flex items-center hover:animate-bounce lg:text-xl tracking-tight font-[900] bg-clip-text text-transparent text-base bg-gradient-to-r from-sky-400 to-blue-500 uppercase">
-          <img src={Logo} alt="logo Anime" className="w-8 h-8 sm:h-10 sm:w-10 mr-1" />
+          <img src={Logo} alt="logo Anime" className="w-10 h-8 sm:h-10 sm:w-12 mr-1" />
           <span className="hidden sm:block"> Anime</span>
         </p>
       </Link>
