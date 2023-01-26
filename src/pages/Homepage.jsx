d721a498ebd9d1500b4ce4ebf871082fd7a257bf
firmankhoiril <firmankhoiril13@gmail.com>
2023-01-26 16:21:41 +0700

    lol

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Anime from "../assets/anime.png";
+import Background from "../assets/background.jpg";
 import { useStateContext } from "../context/contextApi";
 import { useNavigate } from "react-router-dom";
 const instagram = "https://www.instagram.com/firman.khoiril/";
@@ -15,26 +16,25 @@ const Homepage = () => {
   };
 
   return (
-    <div className="h-full -mt-[64px] md:-mt-[72px]">
-      <div className="hero h-full bg-no-repeat bg-cover bg-center">
-        <div className="flex items-center justify-evenly md:justify-around md:flex-row flex-col bg-black/40 h-screen text-white">
-          <div className="">
-            <h1 className={`${standar}`}>Welcome To Our Anime Application</h1>
-            <h2 className="text-lg md:text-2xl text-center md:text-left ">Make Sure To Enjoy!</h2>
-            <p className="text-gray-300"> thank you for visiting my website</p>
-            <p className="text-sm tracking-wide">
-              <span>Created By </span>
-              <a href={instagram} target="_blank" className="text-transparent text-lg font-bold bg-clip-text bg-gradient-to-r from-fuchsia-500 to-red-500 via-pink-500">
-                Firman Khoiril Rohmatullah
-              </a>
-            </p>
-          </div>
-          <div className="flex flex-col">
-            <img src={Anime} alt="Anime" className="h-96 w-62 mb-" />
-            <button className={`text-xl bg-black/60 px-4 py-3 hover:bg-black/80 rounded-sm`} onClick={toHomepage}>
-              Explore Now !
-            </button>
-          </div>
+    <div className="h-full relative -mt-[64px] md:-mt-[72px]">
+      <div className="flex items-center justify-evenly md:justify-around md:flex-row flex-col bg-black/40 h-screen text-white">
+        <img src={Background} alt="Background" className="h-screen bg-no-repeat bg-cover bg-center relative w-full" />
+        <div className="absolute top-32 md:top-[450px] md:left-40">
+          <h1 className={`${standar}`}>Welcome To Our Anime Application</h1>
+          <h2 className="text-lg md:text-2xl text-center md:text-left ">Make Sure To Enjoy!</h2>
+          <p className="text-gray-300"> thank you for visiting my website</p>
+          <p className="text-sm tracking-wide">
+            <span>Created By </span>
+            <a href={instagram} target="_blank" className="text-transparent text-lg font-bold bg-clip-text bg-gradient-to-r from-fuchsia-500 to-red-500 via-pink-500">
+              Firman Khoiril Rohmatullah
+            </a>
+          </p>
+        </div>
+        <div className="flex flex-col md:mt-0 mt-40 md:right-56 absolute ">
+          <img src={Anime} alt="Anime" className="h-96 w-62 mb-" />
+          <button className={`text-xl bg-black/60 px-4 py-3 hover:bg-black/80 rounded-sm`} onClick={toHomepage}>
+            Explore Now !
+          </button>
         </div>
       </div>
     </div>
