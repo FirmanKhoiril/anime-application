1c304f0bceabae2a2d2cfb137c701b39035cad2d
firmankhoiril <firmankhoiril13@gmail.com>
2023-02-18 12:44:41 +0700

    change bg

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import Background from "../assets/reals.jpg";
+import Anime from "../assets/hehe.jpg";
+import Background from "../assets/mains.png";
 import { useStateContext } from "../context/contextApi";
 import { useNavigate } from "react-router-dom";
 const instagram = "https://www.instagram.com/firman.khoiril/";
@@ -15,28 +16,29 @@ const Homepage = () => {
   };
 
   return (
-    <div className="md:h-screen h-full flex lg:flex-row flex-col w-full overflow-hidden ">
-      <div className="lg:w-[45%] h-[90%] lg:h-full md:h-[50%]">
-        <div className="w-full h-[90%] md:h-full">
-          <img src={Background} alt="background" className="h-full w-full" />
-        </div>
-      </div>
-      <div className="lg:w-[65%] px-4 flex-col flex md:items-center bg-black/90 py-10 h-full md:h-screen">
-        <p className="text-slate-200 bg-black/10">
-          Created By{" "}
-          <a href={instagram} target="_blank" className="text-sky-500">
-            {" "}
-            Firman Khoiril
-          </a>{" "}
-        </p>
-        <div className="bg-white/10 flex flex-col mx-auto items-center justify-center h-[25%] w-80 text-white ">
-          <div className="flex flex-col py-4 w-full h-full m-auto">
-            <button className="py-2 px-4 m-2 bg-white text-black hover:bg-white/90 font-bold rounded-sm">Sign In</button>
-            <button className="py-2 px-4 m-2 bg-white text-black hover:bg-white/90 font-bold rounded-sm">Sign Up</button>
-            <button className="py-2 px-4 m-2 border border-sky-500 hover:bg-sky-500  font-semibold rounded-sm" onClick={toHomepage}>
-              Explore App
-            </button>
+    <div className="h-screen relative overflow-hidden ">
+      <div className="flex items-center justify-evenly md:justify-around md:flex-row flex-col h-screen text-white">
+        <img src={Background} alt="background" className="h-full relative w-full " />
+        <div className="absolute top-[70px] mt-6 md:mt-3 sm:top-16 lg:top-[400px] md:left-40">
+          <h1 className={`${standar} mx-1 font-extrabold sm:font-semibold`}>
+            Welcome To <span className={`bg-gradient-to-r text-transparent  from-sky-500 bg-clip-text via-purple-500  to-blue-500 ${standar}`}>Anime Lovers</span>{" "}
+          </h1>
+          <div className="text-sm mt-0 md:mt-10 text-gray-300 tracking-wide md:text-left text-center">
+            <span>Created By </span>
+            <a
+              href={instagram}
+              target="_blank"
+              className="text-transparent text-[15px] font-bold bg-clip-text bg-gradient-to-r from-fuchsia-500 to-red-500 hover:border-b hover:border-white via-pink-500 hover:to-fuchsia-500 hover:from-red-500 hover:via-pink-500"
+            >
+              Firman Khoiril Rohmatullah
+            </a>
           </div>
+        </div>
+        <div className="flex flex-col md:mt-0 sm:mt-20 mt-[180px] xs:mt-[200px] md:right-56 absolute ">
+          <img src={Anime} alt="Anime" className="h-[270px] md:mt-20 sm:h-[300px] md:h-96 w-62 borders" />
+          <button className={`text-xl bg-black/60 px-3 py-3 hover:bg-black/80 rounded-md`} onClick={toHomepage}>
+            Explore Now !
+          </button>
         </div>
       </div>
     </div>
